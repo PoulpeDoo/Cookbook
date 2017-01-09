bed75de7b511fd6079954a34b70442d88ec61143
PoulpeDoo <motorbike9618@gmail.com>
2017-01-09 15:58:22 +0300

    Revert "TestCommit"
    This reverts commit 61f59f2f399fb39e26504b68c6224b780369cae2.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace Cookbook
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            MessageBox.Show("Hello world");
         }
     }
 }
