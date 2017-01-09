61f59f2f399fb39e26504b68c6224b780369cae2
PoulpeDoo <motorbike9618@gmail.com>
2017-01-09 15:07:58 +0300

    TestCommit

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace Cookbook
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            MessageBox.Show("Hello world");
         }
     }
 }
