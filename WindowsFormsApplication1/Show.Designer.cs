ac15025bb6bc6fdb5720f82b22cb566ae38dea40
makcemycsh <m.shilo.v@yandex.ru>
2017-01-20 01:47:34 +0300

    AddRecepy

--- WindowsFormsApplication1/Show.Designer.cs
+++ WindowsFormsApplication1/Show.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(292, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кулинарные рецепты";
             // 
             // Show
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(867, 587);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(838, 532);
+            this.Controls.Add(this.label1);
             this.Name = "Show";
             this.Text = "Show";
             this.Load += new System.EventHandler(this.Show_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsApplication1/Show.cs
+++ WindowsFormsApplication1/Show.cs
@@ -14,65 +14,91 @@ namespace WindowsFormsApplication1
     public partial class Show : Form
 
     {
+        string result = "";
+        public string id;
+        Form f;
         SqlConnection SqlConnection;
-        public Show()
+           public Show()
         {
             InitializeComponent();
         }
 
-        private void Show_Load(object sender, EventArgs e)
+       
+        
+         public void Show_Load(object sender, EventArgs e)
         {
             string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
             SqlConnection = new SqlConnection(connectionString);
             
             
             SqlCommand command = new SqlCommand("SELECT * FROM [Table]", SqlConnection);
-          //  string result = "";
-          int top1 = 50;
-          int left1 = 100;
-            int top2 = 50;
-            int left2 = 255;
+          
+          int toppb = 85;
+          int leftpb = 100;
+          int topbt = 85;
+          int leftbt = 410;
+          
 
             using (SqlConnection)
             {
                 SqlConnection.Open();
                 SqlDataReader reader = command.ExecuteReader();
-
+                //id = Convert.ToString(reader["Name"]);
 
                 while (reader.Read())
                 {
-                    ListBox listbox = new ListBox();
-                    listbox.Left = left2;
-                    listbox.Top = top2;
-                    listbox.Height = 82;
-                    listbox.Width = 550;
-                    Button button = new Button();
-                    button.Left = left1;
-                    button.Top = top1;
-                    button.Height = 82;
-                    button.Width = 150;
-                    // button.TextAlign = MiddleCenter;
-                    //   result += Convert.ToString(reader["Name"]);
-                    listbox.Items.Add(Convert.ToString(reader["Opicanie"]));
-                    button.Text = Convert.ToString(reader["Name"]);
-                    this.Controls.Add(listbox);
-                    top2 += listbox.Height + 2;
-                    this.Controls.Add(button);
-                    top1 += button.Height + 2;
+
+                    Button bt = new Button();
+                    bt.Left = leftbt;
+                    bt.Top = topbt;
+                    bt.Height = 40;
+                    bt.Width = 350;
+                    bt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    bt.Text = Convert.ToString(reader["Name"]);
+                    bt.Tag = Convert.ToString(reader["id"]);
+                    this.Controls.Add(bt);
+                    topbt += bt.Height + 170;
+
+
+
+
+
+                    PictureBox pb = new PictureBox();
+                    pb.Left = leftpb;
+                    pb.Top = toppb;
+                    pb.Height = 200;
+                    pb.Width = 300;
+                    pb.ImageLocation = Convert.ToString(reader["ImgUrl"]);
+                    this.Controls.Add(pb);
+                    toppb += pb.Height + 10;
+                   
                     
+                    bt.Click += new System.EventHandler(resepy);
+                                 
+
+                    // button.TextAlign = MiddleCenter;
+                   result += Convert.ToString(reader["Id"]);
+
                 }
-               // 
-                // Закрыть DataReader
-                reader.Close();
+          
+              reader.Close();
             }
-           
+        }
+
+
+        public void resepy(object sender , EventArgs e)
+        {
+
+            MessageBox.Show(Convert.ToString((sender as Button).Tag));
+            int IdResepy = Int32.Parse(Convert.ToString((sender as Button).Tag));
+            f = new OpenResepy();
+            f.Show();
+
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
 
         }
 
-                         
-              
-           
-                  
-        
     }
 }
